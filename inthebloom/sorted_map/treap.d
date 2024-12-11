5e460fb67208266ddc018f976d9cf771ebea3c54
InTheBloom <nato.rider.smm2@gmail.com>
2024-12-11 17:23:14 +0900

    外部公開向けのsplit

--- inthebloom/sorted_map/treap.d
+++ inthebloom/sorted_map/treap.d
@@ -38,39 +38,48 @@ class Treap (K, V): SortedMap!(K, V)
         return root.size;
     }
 
-    static node_ptr merge (node_ptr lt, node_ptr rt) {
+    static node_ptr internal_merge (node_ptr lt, node_ptr rt) {
         if (lt == null || rt == null) return lt == null ? rt : lt;
         import std.algorithm: swap;
         if ((*rt.value)[0] < (*lt.value)[0]) swap(lt, rt);
 
         if (rt.priority < lt.priority) {
             lt.size += rt.size;
-            auto rsub = split(rt, (*lt.value)[0]);
-            lt.left = merge(lt.left, rsub[0]);
-            lt.right = merge(lt.right, rsub[1]);
+            auto rsub = internal_split(rt, (*lt.value)[0]);
+            lt.left = internal_merge(lt.left, rsub[0]);
+            lt.right = internal_merge(lt.right, rsub[1]);
             return lt;
         }
 
         rt.size += lt.size;
-        auto lsub = split(lt, (*rt.value)[0]);
-        rt.left = merge(rt.left, lsub[0]);
-        rt.right = merge(rt.right, lsub[1]);
+        auto lsub = internal_split(lt, (*rt.value)[0]);
+        rt.left = internal_merge(rt.left, lsub[0]);
+        rt.right = internal_merge(rt.right, lsub[1]);
         return rt;
     }
 
-    static Tuple!(node_ptr, node_ptr) split (node_ptr t, K bound) {
+    static Tuple!(node_ptr, node_ptr) internal_split (node_ptr t, K bound) {
         if (t == null) return tuple(t, t);
         if (bound <= (*t.value)[0]) {
-            auto v = split(t.left, bound);
+            auto v = internal_split(t.left, bound);
             if (v[0] != null) t.size -= v[0].size;
             t.left = v[1];
             return tuple(v[0], t);
         }
 
-        auto v = split(t.right, bound);
+        auto v = internal_split(t.right, bound);
         if (v[1] != null) t.size -= v[1].size;
         t.right = v[0];
         return tuple(t, v[1]);
+    }
+
+    // 外部向け
+    Treap!(K, V) split (K x) {
+        auto v = internal_split(root, x);
+        root = v[0];
+        auto res = new Treap!(K, V)();
+        res.root = v[1];
+        return res;
     }
 
     bool insert (immutable K x, V v) {
@@ -86,19 +95,19 @@ class Treap (K, V): SortedMap!(K, V)
         new_node.size = 1;
         new_node.value = new value(x, v);
 
-        root = merge(root, new_node);
+        root = internal_merge(root, new_node);
         return true;
     }
 
     bool remove (immutable K x) {
-        auto t1 = split(root, x);
+        auto t1 = internal_split(root, x);
         if (x == K.max) {
             root = t1[0];
             return t1[1] != null;
         }
 
-        auto t2 = split(t1[1], x + 1);
-        root = merge(t1[0], t2[1]);
+        auto t2 = internal_split(t1[1], x + 1);
+        root = internal_merge(t1[0], t2[1]);
         return t2[0] != null;
     }
 
